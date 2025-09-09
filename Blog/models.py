c5d8bad536a5b4402fc2496c50e961514c2929a8
sam-22-dev <sam-22-dev@gmail,com>
2025-09-10 00:00:02 +0300

    add static & media settings , image field

--- Blog/models.py
+++ Blog/models.py
@@ -13,3 +13,8 @@ class Post(models.Model):
     draft = models.BooleanField(default=True)
     tags = TaggableManager()
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True,related_name='Post_User')
+    image = models.ImageField(upload_to='posts')
+
+    def __str__(self):
+        return self.title
+    
